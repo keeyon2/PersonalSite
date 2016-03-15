48053eaf0e8628a4771080f9fdb574eaf32ccc6b
I <Keeyon@172-16-181-128.DYNAPOOL.NYU.EDU>
2016-03-15 15:34:34 -0400

    Finished with bootstrap version

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webapp',
+    'personal',
+    'blog',
+    'projects',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
